4db43b68e0e52e3d24b435f6dc2a92af0e20b799
Gonzalo Ruiz <gonzalo.ruiz.anabalon@gmail.com>
2024-05-27 19:01:50 -0400

    termino proyecto festival de musica

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,34 @@
-import { src, dest, watch } from 'gulp'
-import * as dartSass from 'sass'
-import gulpSass from 'gulp-sass'
+import { src, dest, watch, series } from "gulp";
+import * as dartSass from "sass";
+import gulpSass from "gulp-sass";
 
 // export function hola(done) {
 // 	console.log("Hola desde Gulpfile.js");
-// 	// funcion que finaliza le avisa gulpfile 
+// 	// funcion que finaliza le avisa gulpfile
 // 	done();
 // }
 
-const sass = gulpSass(dartSass)
+const sass = gulpSass(dartSass);
+
+export function js(done) {
+  src("src/js/app.js").pipe(dest("build/js"));
+
+  done();
+}
 
 export function css(done) {
-	// pipes
-	src('src/scss/app.scss').pipe( sass()).pipe( dest('build/css'))
-	
-	done()
+  // pipes
+  src("src/scss/app.scss", { sourcemaps: true })
+    .pipe(sass().on('error', sass.logError) )
+    .pipe(dest("build/css"), {sourcemaps: '.'});
+
+  done();
 }
 
 export function dev() {
-	watch('src/scss/**/*.scss', css)
+  watch("src/scss/**/*.scss", css);
+  watch("src/js/**/*.js", js);
 }
+
+export default series(js, css, dev);
+// puede usar parallel se puede usar en el cual arrancas todas las tareas de una
