0bb51a0ddcf8e6031020ef555ba2caa06c74e55c
Gonzalo Ruiz <gonzalo.ruiz.anabalon@gmail.com>
2024-05-28 22:05:49 -0400

    pryecto festiva terminado con performance web

--- src/js/app.js
+++ src/js/app.js
@@ -25,9 +25,17 @@ function crearGaleria() {
   const galeria = document.querySelector(".galeria-imagenes");
 
   for (let i = 1; i <= CANTIDAD_IMAGEN; i++) {
-    const imagen = document.createElement("IMG");
-    imagen.src = `src/img/gallery/full/${i}.jpg`;
-    imagen.alt = "imagen galeria";
+    const imagen = document.createElement("PICTURE");
+    imagen.innerHTML = `
+    <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+`;
+    // imagen.loading = "lazy";
+    // imagen.width = "300";
+    // imagen.height = "200";
+    // imagen.src = `src/img/gallery/thumb/${i}.jpg`;
+    // imagen.alt = "imagen galeria";
 
     // Event handler
     imagen.onclick = function () {
@@ -39,9 +47,14 @@ function crearGaleria() {
 }
 
 function mostrarImagen(i) {
-  const imagen = document.createElement("IMG");
-  imagen.src = `src/img/gallery/full/${i}.jpg`;
-  imagen.alt = "imagen galeria";
+  const imagen = document.createElement("PICTURE");
+  imagen.innerHTML = `
+    <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+    <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+    <img loading="lazy" width="200" height="300" src="build/img/gallery/full/${i}.jpg" alt="imagen galeria">
+`;
+  // imagen.src = `src/img/gallery/full/${i}.jpg`;
+  // imagen.alt = "imagen galeria";
   // Generar modal
   const modal = document.createElement("DIV");
   modal.classList.add("modal");
